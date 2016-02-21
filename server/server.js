4c638a9652beac05a7e06f3159c27ac45a178971
Ben <ishmandoo@gmail.com>
2016-02-21 17:34:02 -0500

    added some test data to the server"

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ var app = express();
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 
+app.get('/api/products', function (req, res) {
+  res.json({products: [{name: "Dog", description: "A dog."}, {name: "Deluxe Dog", description: "A better dog."}]});
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
